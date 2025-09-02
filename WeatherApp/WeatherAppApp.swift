3864333bdb6148aa2ad2b491584466d193a12e11
Erin Sarlak <erinsarlak003@gmail.com>
2025-09-03 00:47:37 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Erin Şarlak on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
